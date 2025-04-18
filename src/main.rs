dcf1d25bc796bfd049dc75f9c360d35f7e7b4e21
neko-04 <sateraito3696@gmail.com>
2025-04-19 07:40:03 +0900

    37ページまで

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use std::io;
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
 
 fn main() {
     println!("数を当ててね");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("秘密の数字は次の通り：{}", secret_number);
+
     println!("予想を入力してね");
 
     let mut guess = String::new();
@@ -10,5 +17,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("読み込みに失敗しました。");
 
-    println!("次のように予想しました！：{}", guess)
+    let guess: u32 = guess.trim().parse()
+
+        .expect("数値を入力してね！");
+
+    println!("次のように予想しました！：{}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("小さすぎ!"),
+        Ordering::Greater => println!("大きすぎ！"),
+        Ordering::Equal => println!("やったね！"),
+    }
 }
