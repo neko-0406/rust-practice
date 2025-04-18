64e830ee3b0163c887c7afc378ab75824497fadb
neko-04 <sateraito3696@gmail.com>
2025-04-18 23:53:32 +0900

    いろいろ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("数を当ててね");
+    println!("予想を入力してね");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("読み込みに失敗しました。");
+
+    println!("次のように予想しました！：{}", guess)
 }
