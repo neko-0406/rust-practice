c8dafedb57888c88d46b742a39cad075b73568a5
neko-04 <sateraito3696@gmail.com>
2025-04-19 16:39:57 +0900

    ループとエラー処理

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,31 @@ fn main() {
 
     println!("秘密の数字は次の通り：{}", secret_number);
 
-    println!("予想を入力してね");
+    loop {
+        println!("予想を入力してね");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("読み込みに失敗しました。");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("読み込みに失敗しました。");
 
-    let guess: u32 = guess.trim().parse()
-
+        let guess: u32 = guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }
         .expect("数値を入力してね！");
 
-    println!("次のように予想しました！：{}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("小さすぎ!"),
-        Ordering::Greater => println!("大きすぎ！"),
-        Ordering::Equal => println!("やったね！"),
+        println!("次のように予想しました！：{}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さすぎ!"),
+            Ordering::Greater => println!("大きすぎ！"),
+            Ordering::Equal => {
+                println!("やったね！");
+                break;
+            },
+        }
     }
+    
 }
