a2c31b7e6c7bb04efc5d68a28c60f97de23fc50a
neko-04 <sateraito3696@gmail.com>
2025-04-19 16:54:20 +0900

    main.rs削除

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,6 @@ use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
-    println!("数を当ててね");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("秘密の数字は次の通り：{}", secret_number);
-
-    loop {
-        println!("予想を入力してね");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("読み込みに失敗しました。");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("次のように予想しました！：{}", guess);
-    
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("小さすぎ!"),
-            Ordering::Greater => println!("大きすぎ！"),
-            Ordering::Equal => {
-                println!("やったね！");
-                break;
-            },
-        }
-    }
     
 }
